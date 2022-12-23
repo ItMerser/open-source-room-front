8dfd3fe10b1cbbecf4f62791c72a7abada0952f8
ItMerser <alekseymaksimovich4@yandex.ru>
2022-12-24 02:42:24 +0300

    added dropdown menu for NavBar

--- src/components/common/NavBar/NavBar.tsx
+++ src/components/common/NavBar/NavBar.tsx
@@ -1,22 +1,102 @@
-import React, {FC} from 'react'
+import React, {FC, MouseEvent, useState} from 'react'
 import {Link} from 'react-router-dom'
-import {AppBar, Toolbar, Button} from '@mui/material'
+import {
+    AppBar,
+    Toolbar,
+    Button,
+    Menu,
+    MenuItem,
+    Avatar,
+    Divider,
+    ListItemIcon,
+    Tooltip
+} from '@mui/material'
+import Logout from '@mui/icons-material/Logout'
+import {logout} from 'utils/auth'
 import {PAGE} from 'routing'
 import {useAppSelector} from 'store/config'
 import {BACKGROUND_COLOR, TEXT_COLOR} from 'const/styles'
 
 const NavBar: FC = () => {
     const {isAuthenticated, nickname} = useAppSelector(state => state.specialistReducer)
+
+    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
+    const open = Boolean(anchorEl)
+    const handleClick = (event: MouseEvent<HTMLElement>) => setAnchorEl(event.currentTarget)
+
+    const handleClose = () => setAnchorEl(null)
+
     return (
         <AppBar position="static" sx={styles.appBar}>
             <Toolbar sx={styles.toolBar}>
                 <Button component={Link} to={PAGE.HOME} sx={styles.button}>APP</Button>
                 {
                     isAuthenticated
-                        ? <Button component={Link} to="" sx={styles.button}>{nickname}</Button>
+                        ? <Tooltip title="menu">
+                            <Button
+                                component={Link}
+                                to=""
+                                sx={styles.button}
+                                onClick={handleClick}
+                                size="small"
+                                aria-controls={open ? 'account-menu' : undefined}
+                                aria-haspopup="true"
+                                aria-expanded={open ? 'true' : undefined}
+                            >
+                                {nickname}
+                            </Button>
+                        </Tooltip>
                         : <Button component={Link} to={PAGE.AUTHENTICATION} sx={styles.button}>SIGN IN</Button>
                 }
             </Toolbar>
+
+            <Menu
+                anchorEl={anchorEl}
+                id="account-menu"
+                open={open}
+                onClose={handleClose}
+                onClick={handleClose}
+                PaperProps={{
+                    elevation: 0,
+                    sx: {
+                        overflow: 'visible',
+                        filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+                        mt: 1.5,
+                        '& .MuiAvatar-root': {
+                            width: 32,
+                            height: 32,
+                            ml: -0.5,
+                            mr: 1,
+                        },
+                        '&:before': {
+                            content: '""',
+                            display: 'block',
+                            position: 'absolute',
+                            top: 0,
+                            right: 14,
+                            width: 10,
+                            height: 10,
+                            bgcolor: 'background.paper',
+                            transform: 'translateY(-50%) rotate(45deg)',
+                            zIndex: 0,
+                        },
+                    },
+                }}
+                transformOrigin={{horizontal: 'right', vertical: 'top'}}
+                anchorOrigin={{horizontal: 'right', vertical: 'bottom'}}
+            >
+                <MenuItem component={Link} to="">
+                    <Avatar/>
+                    Profile
+                </MenuItem>
+                <Divider/>
+                <MenuItem component="button" onClick={logout}>
+                    <ListItemIcon>
+                        <Logout fontSize="small"/>
+                    </ListItemIcon>
+                    Logout
+                </MenuItem>
+            </Menu>
         </AppBar>
     )
 }
